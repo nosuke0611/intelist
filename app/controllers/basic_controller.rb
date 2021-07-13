a3d1fe3f835208984014b01f1f41fa9f32ead5bb
nosuke0611 <n.nosuke0611@gmail.com>
2021-07-14 00:18:04 +0900

    新規投稿用モーダルの追加
    新規投稿を投稿用ページからではなくトップページ等のモーダル上で作成できるよう修正

--- app/controllers/basic_controller.rb
+++ app/controllers/basic_controller.rb
@@ -2,8 +2,10 @@ class BasicController < ApplicationController
   def home
     if user_signed_in?
       @posts = Post.where(user_id: [current_user.id, *current_user.following_ids]).page(params[:page]).per(20)
+      @post = Post.new
     else
       @posts = Post.new
+      @post = Post.new
     end
   end
 end
