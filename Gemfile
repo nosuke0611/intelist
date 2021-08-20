a9983c186ca66f0382b7d53d5aa1421cb31b6bc0
nosuke0611 <n.nosuke0611@gmail.com>
2021-08-21 01:15:51 +0900

    Capistranoの導入
    Capistranoのインストールおよび設定を実施

--- Gemfile
+++ Gemfile
@@ -44,11 +44,15 @@ gem 'bootsnap', '>= 1.4.4', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem "rspec-rails"
-  gem "factory_bot_rails"
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
   gem 'bullet'
   gem 'rubocop', require: false
   gem 'rubocop-rails'
+  gem 'capistrano'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rails'
+  gem 'capistrano-rbenv'
 end
 
 group :development do
--- config/unicorn/unicorn.prod.rb
+++ config/unicorn/unicorn.prod.rb
@@ -1,32 +1,28 @@
-# set lets
-$worker = 2
-$timeout = 30
-rails_env = ENV['RAILS_ENV'] || 'development'
-if rails_env == 'production'
-  $app_dir = "/var/www/rails/intelist"
-else
-  $app_dir = "/intelist"
-end
-$listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
+# setlist
+$app_dir = '/var/www//rails/intelist/current'
+$listen = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
+$pid = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
-# set config
-worker_processes  $worker
+
+# setconfig
+worker_processes 2
 working_directory $app_dir
 stderr_path $std_log
 stdout_path $std_log
-timeout $timeout
+timeout 30
 listen  $listen
 pid $pid
+
 # loading booster
 preload_app true
+
 # before starting processes
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
     begin
-      Process.kill "QUIT", File.read(old_pid).to_i
+      Process.kill 'QUIT', File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
