79a921ef88e0502356f38c3802378c50846c7484
nosuke0611 <n.nosuke0611@gmail.com>
2021-10-16 22:55:49 +0900

    ソート機能回りのリファクタリング
    コードの一部が重複していたためリファクタリングを実施。

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,9 +2,9 @@ class TagsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tag_search_params = tag_search_params
+    @search_params = tag_search_params
     @all_tags = Tag.joins(:post_tag_maps).group(:tag_id)
-    @tags = @all_tags.searched(@tag_search_params).page(params[:page]).per(40).order("#{tags_sort_column} #{tags_sort_direction}")
+    @tags = @all_tags.searched(@search_params).page(params[:page]).per(40).order("#{tags_sort_column} #{sort_direction}")
   end
 
   # タグ一覧ソート用メソッド（デフォルトは投稿数降順）
@@ -16,17 +16,7 @@ class TagsController < ApplicationController
     end
   end
 
-  # タグ一覧ソート方向変更用メソッド
-  def tags_sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : 'desc'
-  end
-
-  # ソート時に検索条件を引き継ぐためのメソッド
-  def take_tag_search_params
-    { searched: { tag_name: @tag_search_params.try(:[], :tag_name) } }
-  end
-
-  helper_method :tags_sort_column, :tags_sort_direction, :take_tag_search_params
+  helper_method :tags_sort_column
 
   private
 
--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,8 +2,8 @@ module TagsHelper
   # タグ一覧画面でのソート機能
   def tags_sortable(column, title, hash_params = {})
     title ||= column.titleize
-    css_class = tags_sort_column.include?(column) ? "current_#{tags_sort_direction}" : nil
-    direction = tags_sort_direction == 'asc' ? 'desc' : 'asc'
+    css_class = tags_sort_column.include?(column) ? "current_#{sort_direction}" : nil
+    direction = sort_direction == 'desc' ? 'asc' : 'desc'
     link_to title, { column: column, direction: direction }.merge(hash_params), class: "sort_header #{css_class}", remote: true
   end
 end
